63b4193f714a5cda840672f6b9217ef1cdfb8454
lalith-ae30 <lalithae30@gmail.com>
2024-10-06 01:48:18 +0530

    Formatting

--- pos_embedding.py
+++ pos_embedding.py
@@ -1,10 +1,7 @@
 import torch
 from torch import nn
-<<<<<<< HEAD
 import math
-=======
-import numpy as np
->>>>>>> d245aaf263e5ee941acf8607e576e89c31b949a0
+
 
 class SinusoidalPositionEmbeddings(nn.Module):
     def __init__(self, dim):
@@ -14,11 +11,7 @@ class SinusoidalPositionEmbeddings(nn.Module):
     def forward(self, time):
         device = time.device
         half_dim = self.dim // 2
-<<<<<<< HEAD
         embeddings = math.log(10000.0) / (half_dim - 1)
-=======
-        embeddings = np.log(10000.0) / (half_dim - 1)
->>>>>>> d245aaf263e5ee941acf8607e576e89c31b949a0
         embeddings = torch.exp(torch.arange(half_dim, device=device) * -embeddings)
         embeddings = time[:, None] * embeddings[None, :]
         embeddings = torch.cat((embeddings.sin(), embeddings.cos()), dim=-1)
