Improve model loading (hack) Clip sampling steps to fix high variance failure of cosine schedule

forward.py
@@ -1,14 +1,12 @@
-from einops import rearrange
-import numpy as np
 import torch
 import torch.nn.functional as F
 
-from utils import extract, to_rgb
-from config import DiffusionConfig, create_diffusion_config
+from utils import extract
+from scheduler import Schedules
 
 
 # forward diffusion
-def q_sample(x_start, t, config: DiffusionConfig, noise=None):
+def q_sample(x_start, t, config: Schedules, noise=None):
     if noise is None:
         noise = torch.randn_like(x_start)
 
@@ -20,14 +18,11 @@ def q_sample(x_start, t, config: DiffusionConfig, noise=None):
     return sqrt_alphas_cumprod_t * x_start + sqrt_one_minus_alphas_cumprod_t * noise
 
 
-from PIL import Image
-
-
 def p_losses(
     denoise_model,
     x_start,
     t,
-    config: DiffusionConfig,
+    config: Schedules,
     noise=None,
     loss_type="l1",
     debug=False,
@@ -38,33 +33,6 @@ def p_losses(
     x_noisy = q_sample(x_start=x_start, t=t, config=config, noise=noise)
     predicted_noise = denoise_model(x_noisy, t)
 
-    if debug:
-        im = [
-            rearrange(
-                to_rgb(x_start),
-                "b c h w-> h (b w) c",
-            )
-        ]
-        im.append(
-            rearrange(
-                to_rgb(x_noisy),
-                "b c h w-> h (b w) c",
-            )
-        )
-        im.append(
-            rearrange(
-                to_rgb(predicted_noise-noise),
-                "b c h w-> h (b w) c",
-            )
-        )
-        im.append(
-            rearrange(
-                to_rgb(noise-noise),
-                "b c h w-> h (b w) c",
-            )
-        )
-        Image.fromarray(np.concatenate(im)).save("dbg.png")
-
     if loss_type == "l1":
         loss = F.l1_loss(noise, predicted_noise)
     elif loss_type == "l2":
@@ -74,4 +42,6 @@ def p_losses(
     else:
         raise NotImplementedError()
 
-    return loss
+    if debug:
+        return (loss, x_start, x_noisy, predicted_noise, noise)
+    return (loss,)

sample.py
@@ -1,13 +1,15 @@
+import logging
 import torch
 
 from tqdm.auto import tqdm
 
-from config import DiffusionConfig
+from scheduler import Schedules
 from utils import extract
 
+logger = logging.getLogger(__name__)
 
 @torch.no_grad()
-def p_sample(model, x, t, t_index, config: DiffusionConfig):
+def p_sample(model, x, t, t_index, config: Schedules) -> torch.Tensor:
     betas_t = extract(config.betas, t, x.shape)
     sqrt_one_minus_alphas_cumprod_t = extract(
         config.sqrt_one_minus_alphas_cumprod, t, x.shape
@@ -31,22 +33,23 @@ def p_sample(model, x, t, t_index, config: DiffusionConfig):
 
 # Algorithm 2 but save all images:
 @torch.no_grad()
-def p_sample_loop(model, shape, config: DiffusionConfig):
+def p_sample_loop(model, shape, config: Schedules):
     device = next(model.parameters()).device
 
     b = shape[0]
     # start from pure noise (for each example in the batch)
     img = torch.randn(shape, device=device)
-    imgs = []
+    imgs = [img.cpu().numpy()]
 
     for i in tqdm(
         reversed(range(0, config.timesteps)),
-        desc="sampling loop time step",
+        desc="Sampling Progress",
         total=config.timesteps,
+        disable=logger.level > logging.INFO
     ):
         img = p_sample(
             model, img, torch.full((b,), i, device=device, dtype=torch.long), i, config
-        )
+        ).clip(-1, 1)
         imgs.append(img.cpu().numpy())
     return imgs
 
@@ -55,7 +58,7 @@ def p_sample_loop(model, shape, config: DiffusionConfig):
 def sample(
     model,
     image_size,
-    config: DiffusionConfig,
+    config: Schedules,
     batch_size=16,
     channels=3,
 ):